981e523b2f9586f40086d22ed2892573077a90eb
Mathew Fleisch <mathew.fleisch@gmail.com>
2015-04-27 08:37:08 -0700

    Removed old commented out code, and moved functions in retro-fit.sh into functions.sh (and sourced it)

--- functions.sh
+++ functions.sh
@@ -205,3 +205,161 @@ function parse_strip_directory
 	fi
 
 }
+
+
+
+function retrofit_file
+{
+	flag_verbose=$1
+	target_file=$2
+	header_pre_path=$3
+	header_post_path=$4
+	footer_path=$5
+	main_content=""
+	content_toggle=false
+
+
+	#Check that all template files exist
+	all_found=true
+	if [ -f $header_pre_path ]; then
+		#echo "Header(1) exits..."
+		header_pre_contents=`cat $header_pre_path`
+	else
+		echo "Header(1) not found..."
+		all_found=false
+	fi
+
+	if [ -f $header_post_path ]; then
+		#echo "Header(2) exits..."
+		header_post_contents=`cat $header_post_path`
+	else
+		echo "Header(2) not found..."
+		all_found=false
+	fi
+
+	if [ -f $footer_path ]; then
+		#echo "Footer exits..."
+		footer_contents=`cat $footer_path`
+	else
+		echo "Footer not found..."
+		all_found=false
+	fi
+
+	if [ $all_found = true ]; then
+		#echo "Template Files exist! Procede..."
+		new_file_contents="$header_pre_contents"
+	else 
+		echo "$IFS ***************   FATAL ERROR!!   ***************"
+		echo "Missing themplate file!"
+		exit 1
+	fi
+
+	if [ -f $target_file ]; then
+		target_file_source=`cat $target_file`
+		target_file_size=`ls -lah "$target_file" | awk '{ print $5}'` 
+		target_file_lines=`wc -l "$target_file" | sed "s/$regex_match_leading_spaces//" | sed "s/$regex_match_first_space_to_end//"`
+		if [ $flag_verbose == true ]; then
+			echo "$IFS   Target File Before: $target_file_size and $target_file_lines lines"
+		fi
+		for next in `cat $target_file`
+		do	
+			line_number=$((line_number+1))
+			#loopy
+			#echo "$line_number: $next"
+
+			#regex trigger 1 <title>
+			if [[ $next =~ $regex_html_title_pre && $line_number -lt $title_before ]]; then
+				title=`echo "$next" | sed "s/$regex_html_title_pre//" | sed "s/$regex_html_title_post//"`
+				#echo "Title Found: $title"
+				new_file_contents="$new_file_contents$IFS$TAB<title>$title</title>$IFS$header_post_contents"
+			fi
+
+			#regex trigger 2 line 146 'class="container"'
+			if [[ $next =~ $regex_html_container && $line_number -lt $container_location_before ]]; then
+				content_toggle=true
+			fi
+
+			#regex trigger 3 'class="footer"'
+			if [[ $next =~ $regex_html_footer ]]; then
+				content_toggle=false
+				new_file_contents="$new_file_contents$IFS$footer_contents"
+
+			fi
+
+			if [ $content_toggle == true ]; then
+				new_file_contents="$new_file_contents$IFS$next"
+			fi
+
+
+		done
+
+		#echo "$IFS$IFS **************** FILE RETROFIT: ****************$IFS$new_file_contents"
+		echo "$new_file_contents" > $target_file
+		target_file_size_new=`ls -lah "$target_file" | awk '{ print $5}'`
+		target_file_lines_new=`wc -l "$target_file" | sed "s/$regex_match_leading_spaces//" | sed "s/$regex_match_first_space_to_end//"`
+		if [ $flag_verbose == true ]; then
+			target_file_lines_diff=`expr $target_file_lines_new - $target_file_lines`
+			echo "     Number of Lines Difference: $target_file_lines_diff"
+			echo "   Target File After: $target_file_size_new and $target_file_lines_new lines"
+		fi
+	else
+		echo "$IFS ***************   FATAL ERROR!!   ***************"
+		echo "The target file you specified doesn't appear to be a file..."
+		echo "target file: $target_file"
+		exit 1
+	fi
+}
+
+
+
+function retrofit_directory
+{
+	flag_verbose=$1
+	target_directory=$2
+	header_pre_path=$3
+	header_post_path=$4
+	footer_path=$5
+	recursive=$6
+	main_content=""
+	content_toggle=false
+
+	target_found=false
+	output_file=""
+	incs_found=0
+	if [ -d $target_directory ]; then
+		echo "Target directory was found: $target_directory"
+		#if [ $target_directory =~ ^/.*$ ]; then
+		#echo "$(cd $target_directory; pwd)/."
+		#fi
+		echo "    <>------------<          >------------<>"
+		cmd_inc=""
+		if [ $recursive == false ]; then 
+			cmd_inc="-maxdepth 1"
+		fi
+		for target_file in `find "$target_directory" -name '*.html' $cmd_inc`
+		do
+			echo "File Found: $target_file"	
+			#echo "Inject includes here: $target_file"
+			#echo "Includes Path: $include_path"
+			tmp_inc_found=false
+			for test_include in `find "$include_path" -name '*.html'`
+			do
+				if [ $target_file == $test_include ]; then
+					echo "Skip stripping source include: $target_file"
+					tmp_inc_found=true
+				fi
+			done
+			if [ $tmp_inc_found == false ]; then
+				retrofit_res=$( retrofit_file $flag_verbose $target_file $header_pre_path $header_post_path $footer_path )
+				echo "res: $retrofit_res"
+			fi
+			echo "$IFS    <>------------<          >------------<>$IFS"
+		done
+	else
+		echo "$IFS ***************   FATAL ERROR!!   ***************"
+		echo "The target directory you specified doesn't appear to be a directory..."
+		echo "target directory: $target_directory"
+		exit 1
+	fi
+
+}
